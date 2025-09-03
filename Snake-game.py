3fe474a9fd80ca7adcb8ab42b881b30903cca538
moaaadl <moadlobi@gmail.com>
2025-09-03 03:47:01 +0100

    fix a bug

--- Snake-game.py
+++ Snake-game.py
@@ -49,17 +49,18 @@ def move_snake():
         dy = +20
 
     corrdinates = canvas.coords((head))
-    print(corrdinates)
+    
+    # for x
+    if corrdinates[2] > GAME_WIDTH: # right
+        canvas.coords(head, -SPACE_SIZE, corrdinates[1], 0, corrdinates[3])
+    elif corrdinates[0] < 0: # left
+        canvas.coords(head, GAME_WIDTH, corrdinates[1], GAME_WIDTH+SPACE_SIZE, corrdinates[3])
 
-    # the problem is here !
-    if corrdinates[2] >= GAME_WIDTH:
-        canvas.coords(head, 0, corrdinates[1], SPACE_SIZE, corrdinates[3])
-    elif corrdinates[0] <= 0:
-        canvas.coords(head, GAME_WIDTH-SPACE_SIZE, corrdinates[1], GAME_WIDTH, corrdinates[3])
-    elif corrdinates[1] >= GAME_HIGHT:
-        canvas.coords(head, corrdinates[0], 0, corrdinates[2], SPACE_SIZE)
-    elif corrdinates[3] <= 0:
-        canvas.coords(head, corrdinates[0], GAME_HIGHT, corrdinates[2], GAME_HIGHT-SPACE_SIZE)
+    # for y
+    elif corrdinates[3] > GAME_HIGHT: # up
+        canvas.coords(head, corrdinates[0], -SPACE_SIZE, corrdinates[2], 0)
+    elif corrdinates[3] <= 0: # down
+        canvas.coords(head, corrdinates[0], GAME_HIGHT+SPACE_SIZE, corrdinates[2], GAME_HIGHT)
 
 
     canvas.move(head, dx, dy)
